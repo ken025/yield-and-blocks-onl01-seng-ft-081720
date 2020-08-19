cadae14e12881dfe7826514c2041f178cc3a5fcf
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-19 19:57:26 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,3 @@
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
-
 def hello_t(array)
 i = 0 
 
@@ -14,3 +8,8 @@ i = i + 1
   array
 end 
 
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
