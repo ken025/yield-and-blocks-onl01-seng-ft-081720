0ae7b94b6e0c777cf0d0ede5e02fb4be9f368573
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-19 19:57:11 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,3 +1,9 @@
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
+
 def hello_t(array)
 i = 0 
 
@@ -8,8 +14,3 @@ i = i + 1
   array
 end 
 
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
